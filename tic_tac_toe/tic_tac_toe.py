eb9729e3e85916fb97954d1e70725c2c6f41645c
Samino <jadenjabbott@outlook.com>
2023-03-25 19:26:28 -0400

    Bases file for new Tic Tac Toe program. Next commits will include implemented called functions.

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,6 +1,6 @@
 ## GLOBAL VARIABLES
 
-gameStatus = True # true = ongoing, false = stopped
+gameStatus = False # true = ongoing, false = stopped
 numberSet = [1, 2, 3, 4, 5, 6, 7, 8, 9] # reference ints for game board
 currentBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] # game board
 inputVerification = False # global variable for verifying user input
@@ -26,6 +26,9 @@ def updateBoard():
 def drawBoard():
     return None
 
+def gameOver():
+    return None
+
 def endGame():
     return None
 
@@ -34,3 +37,40 @@ def playAgain():
 
 ## BEGIN PROGRAM
 
+gameStatus = True
+resetTerminal()
+
+print("Hello, welcome to Tic Tac Toe.")
+playerMarker = input("Please enter either X or O for your maker: ")
+
+# verify user input & assign the computer the opposite marker
+while inputVerification == False:
+    if playerMarker == "X" or playerMarker == "x":
+        playerMarker = "X"
+        computerMarker = "O"
+        inputVerification = True
+    elif playerMarker == "O" or playerMarker == "o":
+        playerMarker = "O"
+        computerMarker = "X"
+        inputVerification = True
+    else:
+        inputVerification = False
+        playerMarker = input("Incorrect input, please try again: ")
+
+print("")
+print("Starting game...")
+print("")
+
+drawBoard()
+
+while gameStatus == True:
+    getPlayerMove()
+    getComputerMove()
+
+    updateBoard()
+    drawBoard()
+    
+    if gameOver() == True:
+        gameStatus = False
+        endGame()
+        playAgain()
