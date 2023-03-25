985b58d32341390fcd8d6fbbce71f0995a570032
DominikLip <dominik.lipari@gmail.com>
2023-03-25 18:26:32 -0400

    Updated to make deck command work

--- blackjack/BlackJackWIP.py
+++ blackjack/BlackJackWIP.py
@@ -1,10 +1,20 @@
 ## Made by Dominik Lipari
-## functions go in this block 
-def DeckOfCards():
+def DeckOfCards(): # function returns a list to represent a deck of cards
     deck = ["J", "J"]
-    for cardNum in range <= 52:
-            for i in range <= 4:
-                deck.append(str(cardNum))
+    number, kings, queens, jacks = 0, 0, 0, 0
+    while number < 10:
+        i = 0
+        while i < 4:
+             deck.append(str(number))
+             i += 1
+        number += 1
+    while kings < 4:
+        deck.append("K")
+        kings += 1
+    while queens < 4:
+        deck.append("Q")
+        queens += 1
+    while jacks < 4:
+        deck.append("J")
+        jacks += 1
     return deck
-
-print (DeckOfCards)
