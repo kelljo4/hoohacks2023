a0423f7045db909319c8372c537e6b3cacb4634e
Samino <jadenjabbott@outlook.com>
2023-03-25 19:04:31 -0400

    Rewriting the code with a new, cleaner base.

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,86 +1,36 @@
-## SET UP GAME
+## GLOBAL VARIABLES
 
-import os
-print("Hello, welcome to Tic Tac Toe")
-playerType = input("Will you be playing as X or O this game? ")
-computerType = "" # will be set later based on player's input
-correctInput = False # global variable for verifying input
-squareSet = [1, 2, 3, 4, 5, 6, 7, 8, 9] # num set of possible squares
-playerInput = "" # player input variable
-computerInput = "" # computer input variable
+gameStatus = True # true = ongoing, false = stopped
+numberSet = [1, 2, 3, 4, 5, 6, 7, 8, 9] # reference ints for game board
+currentBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] # game board
+inputVerification = False # global variable for verifying user input
+playerMarker = ""
+computerMarker = ""
+playerInput = ""
+computerInput = ""
 
 ## GLOBAL FUNCTIONS
 
-# function for verifying user input
-def verifySquare(input):
-    for i in len(squareSet):
-        try:
-            if int(input) == squareSet(i):
-                return True    
-            else:
-                return False  
-        except:
-                return False
-        
-# function for generating random computer choice of where to place its marker
-def computerPlay(input):
-    # TO DO
+def resetTerminal():
+    return None
 
-    return input
+def getPlayerMove():
+    return None
 
-# function for making the square and resetting the terminal
-def makeSqure(player, computer):
-    # TO DO
+def getComputerMove():
+    return None
 
+def updateBoard():
+    return None
+
+def drawBoard():
+    return None
+
+def endGame():
+    return None
+
+def playAgain():
     return None
 
 ## BEGIN PROGRAM
-
-# verify input data
-while correctInput == False:
-    if playerType == "X" or playerType == "x":
-        computerType = "O"
-        playerType = "X"
-        correctInput = True
-    elif playerType == "O" or playerType == "o":
-        computerType = "X"
-        playerType = "O"
-        correctInput = True
-    else:
-        correctInput = False
-        playerType = input("That is not a valid input! Please try again: ")
-
-# begin game
-print("")
-print("Beginning game now...")
-print("")
-
-print("""-------------------
-|     |     |     |
-|  1  |  2  |  3  |
-|     |     |     |
--------------------
-|     |     |     |
-|  4  |  5  |  6  |
-|     |     |     |
--------------------
-|     |     |     |
-|  7  |  8  |  9  |
-|     |     |     |
--------------------""")
-
-print("")
-playerInput = input("Which squre would you like to place your marker? ")
-
-# verify input
-while verifySquare(playerInput) == True:
-    print("You are placing your marker on square number " + playerInput)
-    computerInput = computerPlay(playerInput)
-    print("The computer is placing its marker on square number " + computerInput)
-
-    makeSquare(playerInput, computerInput)
-
-    
-
-os.system('cls' if os.name == "nt" else 'clear')
 
