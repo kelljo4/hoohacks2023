82c292959a4cd86688963936f87dcbabf994a1ea
DominikLip <dominik.lipari@gmail.com>
2023-03-25 17:31:42 -0400

    Adding blackjack game file

---
+++ blackjack/BlackJackWIP.py
@@ -0,0 +1,16 @@
+## Made by Dominik Lipari, Kellan Jones, Jaden Abbott
+## functions go in this block 
+def DeckOfCards():
+    deck = []
+    for cardNum in range >= 52:
+            for i in range <= 4:
+                deck.append(cardNum)
+
+
+## Text prompt for the name of the game you wish to play insert 
+gameName = input("Input name of game you wish to play")
+if gameName == "war" or gameName == "War":
+    ## function to play game of war
+
+
+
