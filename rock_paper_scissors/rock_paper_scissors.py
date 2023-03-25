8984a3bed54a19312f5bd466dc83e1c3e49a7b95
kelljo4 <keljo4@mail.regent.edu>
2023-03-25 19:00:23 -0400

    Created a basic startup enviorment

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,3 +1,24 @@
-import sys
 import cv2
-img = cv2.imread("rock.png", cv2.IMREAD_ANYCOLOR)
+import pygame
+from pygame.locals import *
+img = cv2.imread("Assets/rock.png")
+
+cv2.imshow('Image', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+pygame.init()
+running = True
+
+screen = pygame.display.set_mode((600, 600))
+
+screen.fill((255, 255, 255))
+pygame.display.update()
+pygame.display.set_caption("Rock Paper Scissors")
+
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+pygame.quit()
