28060f885d4c310aa3445a502f2974cdf611a065
DominikLip <dominik.lipari@gmail.com>
2023-03-25 17:42:06 -0400

    adding Blakcjack file take 2

--- blackjack/BlackJackWIP.py
+++ blackjack/BlackJackWIP.py
@@ -1,16 +1,10 @@
-## Made by Dominik Lipari, Kellan Jones, Jaden Abbott
+## Made by Dominik Lipari
 ## functions go in this block 
 def DeckOfCards():
-    deck = []
-    for cardNum in range >= 52:
+    deck = ["J", "J"]
+    for cardNum in range <= 52:
             for i in range <= 4:
-                deck.append(cardNum)
+                deck.append(str(cardNum))
+    return deck
 
-
-## Text prompt for the name of the game you wish to play insert 
-gameName = input("Input name of game you wish to play")
-if gameName == "war" or gameName == "War":
-    ## function to play game of war
-
-
-
+print (DeckOfCards)
