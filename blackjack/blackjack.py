075ce10c2856026d2db75bb444fc625635f2b828
DominikLip <dominik.lipari@gmail.com>
2023-03-26 01:35:06 -0400

    blackjack refinning

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -6,7 +6,7 @@ import sys
 def MakeDeck(): # function creates a list to represent a deck of cards
     global deck # make the deck list accessible to other functions
     deck = []
-    number, kings, queens, jacks = 0, 0, 0, 0
+    number = 0
     for number in range(13): # 13 values of cards Ace through king
         for i in range(4): # one of each suit 
              deck.append(number)
@@ -20,11 +20,11 @@ def DrawCard(): # draws a card from the deck
 def CheckForBlackjack(hand, pOrD): #checks to see if the hand given is bust or blackjack   pOrD is used to say who owns the hand 
     if sum(hand) == 21:
         print("Blackjack!!", pOrD, "wins!")
-        endPrompt = input("press X + Enter to close the program")
+        input("Press enter to exit program...")
         exit()
     elif sum(hand) > 21:
         print("Bust.", pOrD, "looses.")
-        endPrompt = input("press X + Enter to close the program")
+        input("Press enter to exit program...")
         exit()
 
 pHand, dHand= [], []     
@@ -32,11 +32,11 @@ pHand, dHand= [], []
 def CheckAgainstDealer(): # Checks to see if the player or dealer has more points to determine a winner 
     if sum(pHand) < sum(dHand):
         print("You had ", sum(pHand), ". The dealer had ", sum(dHand), ". You loose.", sep="")
-        endPrompt = input("press X + Enter to close the program")
+        input("Press enter to exit program...")
         exit()
     elif sum(pHand) > sum(dHand):
         print("You had ", sum(pHand), ". The dealer had ", sum(dHand), ". You win!", sep="")
-        endPrompt = input("press X + Enter to close the program")
+        input("Press enter to exit program...")
         exit()
 MakeDeck()
 
